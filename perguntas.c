0b95c77727b647856b0390f2e401c0429603f45e
Farlook <srfarlook20@gmail.com>
2026-02-14 17:06:11 -0300

    Update perguntas.c

--- perguntas.c
+++ perguntas.c
@@ -1,1 +1,27 @@
+int main(){
+ 
+    int idade;
+    float altura;
+    char nome[20];
+    char opcao;
 
+    printf("Olá, Insira sua idade: ");
+    scanf("%d", &idade);
+    printf("Sua idade é: %d\n", idade);
+
+    printf("Agora ensira sua altura: ");
+    scanf("%f", &altura);
+    printf("Sua altura é: %f\n", altura);
+
+    printf("Agora nos diga seu nome: ");
+    scanf("%s", nome);
+    printf("Seu nome é: %s\n", nome);
+
+    printf("Você está satisfeito com o funcionamento? 'S' para sim e 'N' para não: ");
+    scanf(" %c", &opcao);
+    printf("A opção escolhida foi:%c\n", opcao);
+
+    printf("Obrigado por testar meu código!\n");
+
+return 0; 
+}
